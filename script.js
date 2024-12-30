9c55467e83bf28dc5d02b225f28684f906ee7d7d
rameshbhopale02 <ramesh.bhopale2021@gmail.com>
2024-12-30 12:43:46 +0530

    no commit

---
+++ script.js
@@ -0,0 +1,41 @@
+/*
+    faulty calculator
+    + --- -
+    - --- *
+    * --- /
+    / --- +
+    it performs these wrong operations 10% of the time
+*/
+
+// let a,  b, x;
+// a = prompt ("Enter a number");
+// b = prompt ("Enter another number");
+// let op = prompt ("Enter the operation");
+// let cnt = 0;
+// x = Math.floor(Math.random() * 101);
+// a = Number(a);
+// b= Number(b);
+// if(x<10){
+//     if(op==='+'){
+//         console.log(a-b);
+//     }else if(op==='-'){
+//         consol.log(a*b);
+//     }else if(op==='*'){
+//         console.log(a/b);
+
+//     }else if(op==='/'){
+//         console.log(a+b);
+//     }
+// }else{
+//     if(op==='+'){
+//         console.log(a+b);
+//     }else if(op==='-'){
+//         consol.log(a-b);
+//     }else if(op==='*'){
+//         console.log(a*b);
+
+//     }else if(op==='/'){
+//         console.log(a/b);
+//     }
+// }
+
